488cd657ab676877b63343d16f1384437401d72f
sarah roodin <sarahrhoops34@gmail.com>
2021-10-23 17:14:51 -0400

    made another viewcontroller and connected the playButton to the code to make it have rounded corners

--- quiz app real/ViewController.swift
+++ quiz app real/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playButton.layer.cornerRadius = 3
     }
 
 }

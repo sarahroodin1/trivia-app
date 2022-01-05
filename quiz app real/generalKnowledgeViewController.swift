aac71d20d530c5c3f8bb03a17ccc683ee355862b
sarah roodin <sarahrhoops34@gmail.com>
2022-01-05 13:06:32 -0500

    GitHub code for mr. schott

--- quiz app real/generalKnowledgeViewController.swift
+++ quiz app real/generalKnowledgeViewController.swift
@@ -18,21 +18,23 @@ class generalKnowledgeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     //makes a singleton object
-        let questionData1 = Singleton()
-        let randomInt = Int.random(in: 0..<5)
+        let questionData1 = Singleton{ () -> () in
+            randomizer()
+        }
+        let randomInt = 0
         
     //sets the question,a,b,c,&d buttons to the value of the api that is resulted from the randomint
-    func randomizer(){
+        func randomizer(){
         QuestionButton.text = questionData1.arrayOfAPIS[randomInt].question
         AButton.setTitle(questionData1.arrayOfAPIS[randomInt].A, for: .normal)
         BButton.setTitle(questionData1.arrayOfAPIS[randomInt].B, for: .normal)
         CButton.setTitle(questionData1.arrayOfAPIS[randomInt].C, for: .normal)
         DButton.setTitle(questionData1.arrayOfAPIS[randomInt].D, for: .normal)
-    }
     
+        }
        
         //creates a correct answer variable
-        let correctAnswer = questionData1.arrayOfAPIS[randomInt].answer
+        /*let correctAnswer = questionData1.arrayOfAPIS[randomInt].answer
         
         if(correctAnswer == questionData1.arrayOfAPIS[randomInt].A){
             AButton.setTitle("Correct", for: .normal)
@@ -58,15 +60,16 @@ class generalKnowledgeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
+}
+    */
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
 
+
+    }
 }

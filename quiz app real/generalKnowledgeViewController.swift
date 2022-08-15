5abd4e39a102a1ee3edb392c6f7c25b1944175f8
sarah roodin <sarahrhoops34@gmail.com>
2022-08-15 16:40:29 -0400

    Summer work, lots of errors

--- quiz app real/generalKnowledgeViewController.swift
+++ quiz app real/generalKnowledgeViewController.swift
@@ -9,81 +9,35 @@ import UIKit
 
 class GeneralKnowledgeViewController: UIViewController {
     let randomInt = Int.random(in: 0..<548)
+    let correctAnswer = questionData1.arrayOfQuestions[randomInt].answer
+    var questionNumber = 0
 
     @IBAction func ButtonPressed(_ sender:Any){
         var scoreLabel = 0
-          //creates a correct answer variable and changes the button color and text to be green and say 'correct'
-         let correctAnswer = questionData1.arrayOfQuestions[randomInt].answer
-       //if A is correct
-        if(correctAnswer == questionData1.arrayOfQuestions[randomInt].A){
-            AButton.setTitle("Correct", for: .normal)
-            AButton.layer.borderWidth = 20
-
-            AButton.layer.borderColor = UIColor(red: 74/255, green: 218/255, blue: 163/225, alpha: 1).cgColor
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = questionData1.checkAnswer(userAnswer)
+        
+        
+        if(userGotItRight){
+            sender.setTitleColor = UIColor.green
         }
-        //if B is correct
-       else if(correctAnswer == questionData1.arrayOfQuestions[randomInt].B){
-            BButton.setTitle("Correct", for: .normal)
-           BButton.layer.borderWidth = 20
-
-           BButton.layer.borderColor = UIColor(red: 74/255, green: 218/255, blue: 163/225, alpha: 1).cgColor
-        }
-
-        //if C is correct
-        else if(correctAnswer == questionData1.arrayOfQuestions[randomInt].C){
-            CButton.setTitle("Correct", for: .normal)
-            CButton.layer.borderWidth = 20
-
-            CButton.layer.borderColor = UIColor(red: 74/255, green: 218/255, blue: 163/225, alpha: 1).cgColor
-        }
-        //if D is correct
         else{
-        DButton.setTitle("Correct", for: .normal)
-            DButton.layer.borderWidth = 20
-
-            DButton.layer.borderColor = UIColor(red: 74/255, green: 218/255, blue: 163/225, alpha: 1).cgColor
-
+            sender.setTitleColor = UIColor.red
         }
+        questionData1.nextQuestion()
         
-        
-        //if A is incorrect
-         if (correctAnswer != questionData1.arrayOfQuestions[randomInt].A){
-            AButton.setTitle("Incorrect", for: .normal)
-            AButton.layer.borderWidth = 20
-
-            AButton.layer.borderColor = UIColor(red: 200/255, green: 10/255, blue: 10/225, alpha: 1).cgColor
-        }
-        
-        //if B is incorrect
-        else if (correctAnswer != questionData1.arrayOfQuestions[randomInt].B){
-            BButton.setTitle("Incorrect", for: .normal)
-            BButton.layer.borderWidth = 20
-
-            BButton.layer.borderColor = UIColor(red: 200/255, green: 10/255, blue: 10/225, alpha: 1).cgColor
-        }
-        //if C is incorrect
-        else if (correctAnswer != questionData1.arrayOfQuestions[randomInt].C){
-            CButton.setTitle("Incorrect", for: .normal)
-            CButton.layer.borderWidth = 20
-
-            CButton.layer.borderColor = UIColor(red: 200/255, green: 10/255, blue: 10/225, alpha: 1).cgColor
-        }
-        //if D is incorrect
-        else{
-            DButton.setTitle("Incorrect", for: .normal)
-            DButton.layer.borderWidth = 20
-
-            DButton.layer.borderColor = UIColor(red: 200/255, green: 10/255, blue: 10/225, alpha: 1).cgColor
-        }
+     Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         
     }
+       
+        
+    
     @IBOutlet weak var QuestionButton: UILabel!
     @IBOutlet weak var DButton: UIButton!
     @IBOutlet weak var CButton: UIButton!
     @IBOutlet weak var BButton: UIButton!
     @IBOutlet weak var AButton: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
-    let questionData1 = Singleton()
 
     override func viewDidLoad() {
         
@@ -105,15 +59,27 @@ class GeneralKnowledgeViewController: UIViewController {
         DButton.setTitle(questionData1.arrayOfQuestions[randomInt].D, for: .normal)
          
         }
+        
+        //checks if the user answer is correct
+        func checkAnswer(_ userAnswer: String) -> Bool{
+            if(userAnswer == correctAnswer){
+                return true
+                //user is correct
+            }
+           return false
+                //user is incorrect
+        }
+        
+        //changes the question
+        func nextQuestion(){
+            if(questionNumber + 1 < quiz.count){
+                questionNumber += 1
+            }
+            else{
+                questionNumber = 0
+                scoreValue = 0
+            }
+        }
+        
     }
 }
-
-       
-        // Do any additional setup after loading the view.
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
